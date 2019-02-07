d731d53fa35a11760fa3cd473c931a122654484a
Fernando Henrique <liquuid@gmail.com>
2019-02-06 22:43:23 -0200

    added fixture

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+post1 = Post.new
+post1.title = "Cheese"
+post1.kind = "image"
+post1.content = "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.sMjAjvVMYIvrwVI7RNJcKQHaE8%26pid%3D15.1&f=1"
+post1.save
